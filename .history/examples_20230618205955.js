9a39f8fe51212db9ab78baafda6267f6723fd23b
Emil LASLAU <emil.laslau@indsoft.ro>
2023-06-23 20:19:57 +0300

    add diferent changes

--- .history/examples_20230618205955.js
+++
@@ -1,5 +0,0 @@
-var employed = false; // boolean
-var age = 24; // number
-var name = "First Last"; // string
-
-console.info(typeof employed);
--- .history/examples_20230618210023.js
+++
@@ -1,7 +0,0 @@
-var employed = false; // boolean
-var age = 24; // number
-var name = "First Last"; // string
-
-console.info(typeof employed);
-console.info(typeof age);
-console.info(typeof name);

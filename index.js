cdac9846a1de31b86509b17787260c08d10a7a76
Emil LASLAU <emil.laslau@indsoft.ro>
2023-06-25 15:45:29 +0300

    simplifyed 'then' to not use parent funtion

--- index.js
+++ index.js
@@ -43,9 +43,7 @@ function showSkillsList(skills) {
 
 function getSkillsRequest() {
   fetch("skills.json").then(function (r) {
-    r.json().then(function (skills) {
-      showSkillsList(skills);
-    });
+    r.json().then(showSkillsList);
   });
 }
 
